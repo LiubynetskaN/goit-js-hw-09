d24a61102228e73ef4a97f88a07a9631789d85f6
Nataliia Liubynetska <nliubynetska@gmail.com>
2023-04-25 22:39:42 +0300

    hw 9 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startEl.addEventListener('click', startChangeHandler);
+stopEl.addEventListener('click', stopChangeHandler);
+
+let timerId = null;
+function startChangeHandler() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startEl.disabled = true;
+  if (stopEl.hasAttribute('disabled')) {
+    stopEl.disabled = false;
+  }
+}
+
+function startChangeHandler() {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    if (timerId === null) {
+      timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+      }, 1000);
+      startEl.disabled = true;
+      if (stopEl.hasAttribute('disabled')) {
+        stopEl.disabled = false;
+      }
+    }
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', dataHandler);
+
+function dataHandler(event) {
+  event.preventDefault();
+  let currentDelay = 0;
+
+  const delay = Number(event.target.delay.value);
+  const step = Number(event.target.step.value);
+  const amount = Number(event.target.amount.value);
+
+  if (delay < 0) {
+    return Notiflix.Notify.failure(
+      'Value for first step could not be negative'
+    );
+  } else if (amount < 0) { 
+    return Notiflix.Notify.failure('Amount of promises could not be negative');
+  }
+
+  for (let i = 0; i < amount; i += 1) { 
+    currentDelay = delay + step * i;
+
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });  
 }
