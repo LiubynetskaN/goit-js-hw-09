d65685ac50c8192f9d5ed0b85fa8c05861612216
LiubynetskaN <114070841+LiubynetskaN@users.noreply.github.com>
2023-04-26 19:45:06 +0000

    Deploying to gh-pages from @ LiubynetskaN/goit-js-hw-09@c005d3564de8f6b26be4f3e4e48cce74e76ec51f 🚀

--- 01-color-switcher.64a8ca6d.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){n.style.backgroundColor=`${o()}`,null===r&&(r=setInterval((()=>{n.style.backgroundColor=`${o()}`}),1e3),t.disabled=!0,e.hasAttribute("disabled")&&(e.disabled=!1))})),e.addEventListener("click",stopChangeHandler);let r=null;
-//# sourceMappingURL=01-color-switcher.64a8ca6d.js.map
